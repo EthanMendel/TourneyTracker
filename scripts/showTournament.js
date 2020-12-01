a6743e7a2bab1fc07f99c5c76d1da772d4699d99
Ethan Mendel <ethanmendel@Ethans-MBP.attlocal.net>
2020-12-01 15:49:02 -0500

    use query parms to redirect

--- scripts/showTournament.js
+++ scripts/showTournament.js
@@ -1,3 +1,7 @@
-$('#register-team').click(function(){
-    location.href="/registerTeam?tournament_id={{tournament.id}}";
+$(document).ready(function(){
+    $('#register-team').click(function(){
+        //get query parm
+        var tournament_id = getParameterByName('tournament_id');
+        location.href="/registerTeam?tournament_id="+tournament_id;
+    });
 });
